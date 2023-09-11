763415993c9586e394bb6c14511c9e7272a34bf9
Dhanya-R-Hegde <1rn19ec037.dhanyarhegde@gmail.com>
2023-09-11 16:02:14 +0530

    [Dhanya R Hegde] : Added LinkedList UserCase10

---
+++ GenericNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public class GenericNode<T> where T : IComparable<T>
+    {
+        public T Data { get; set; }
+        public GenericNode<T> Next { get; set; }
+
+        public GenericNode(T data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+}
---
+++ SortedLinkedList.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public class SortedLinkedList<T> where T : IComparable<T>
+    {
+        private GenericNode<T> head;
+
+        public void Add(T data)
+        {
+            GenericNode<T> newNode = new GenericNode<T>(data);
+
+            if (head == null || data.CompareTo(head.Data) < 0)
+            {
+                newNode.Next = head;
+                head = newNode;
+            }
+            else
+            {
+                GenericNode<T> current = head;
+
+                while (current.Next != null && data.CompareTo(current.Next.Data) >= 0)
+                {
+                    current = current.Next;
+                }
+
+                newNode.Next = current.Next;
+                current.Next = newNode;
+            }
+        }
+
+        public void Display()
+        {
+            GenericNode<T> current = head;
+
+            while (current != null)
+            {
+                Console.Write(current.Data + " -> ");
+                current = current.Next;
+            }
+
+            Console.WriteLine("null");
+        }
+    }
+}
