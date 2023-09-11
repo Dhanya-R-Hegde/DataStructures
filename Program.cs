b40885f2e523caceb3e3a75aeee15311ada30ec9
Dhanya-R-Hegde <1rn19ec037.dhanyarhegde@gmail.com>
2023-09-11 12:51:22 +0530

    [Dhanya R Hegde] : Added LinkedList UserCase1

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace DataStructures
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        {
+            //UC1
+            LinkedList<int> list1 = new LinkedList<int>();
+            list1.AddFirst(30);
+            list1.AddFirst(56);
+            list1.AddLast(70);
+
+            foreach (int data in list1)
+            {
+                Console.Write(data + " ");
+            }
+
+
+
+        }
+    }
+}
